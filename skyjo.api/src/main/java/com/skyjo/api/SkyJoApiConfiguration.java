757eae29c5676412ef707cb4e8c24c932779be4e
EXPDOM\areyes <areyes@expertime.com>
2023-12-28 13:50:02 +0100

    Config Api project to use core

--- skyjo.api/src/main/java/com/skyjo/api/SkyJoApiConfiguration.java
+++ skyjo.api/src/main/java/com/skyjo/api/SkyJoApiConfiguration.java
@@ -1,5 +1,26 @@
 package com.skyjo.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class SkyJoApiConfiguration {
 
+    @Value("${cors.white-list}")
+    protected String corsWhiteList;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/costumer/**").allowedOrigins(corsWhiteList);
+				registry.addMapping("/account/**").allowedOrigins(corsWhiteList);
+				registry.addMapping("/transaction/**").allowedOrigins(corsWhiteList);
+            }
+        };
+    }
 }

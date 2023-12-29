c1c89ea244d1f0a9cbdcba51f3670b391a9642ac
EXPDOM\areyes <areyes@expertime.com>
2023-12-29 10:03:22 +0100

    Extension methods done

---
+++ skyjo.application/src/main/java/com/skyjo/application/extensions/com/skyjo/core/models/SkyJoSet/SkyJoSetExtensions.java
@@ -0,0 +1,54 @@
+package com.skyjo.application.extensions.com.skyjo.core.models.SkyJoSet;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import com.skyjo.core.models.Player;
+import com.skyjo.core.models.PlayerSet;
+import com.skyjo.core.models.SkyJoSet;
+
+import manifold.ext.rt.api.This;
+
+public class SkyJoSetExtensions {
+
+    public static void initEvaluationBook(@This SkyJoSet set, List<Player> players) {
+        Map<String, Integer> block = new HashMap<>();
+        players.forEach(player -> {
+            if(player.getName() == null)
+                throw new IllegalStateException("Player without name found");
+            block.put(player.getName(), 0);
+        });
+        set.setEvaluationBlock(block);
+    }
+
+    public static boolean hasAnyHiddenCard(@This SkyJoSet set) {
+        return set.getPlayerSets().stream()
+            .anyMatch(ps -> ps.getColumns().stream()
+                .anyMatch(col -> col.getCards().stream()
+                    .anyMatch(card -> !card.getCard().isShown())));
+    }
+
+    public static boolean allCardsAreHidden(@This SkyJoSet set) {
+        return set.getPlayerSets().stream()
+            .allMatch(ps -> ps.getColumns().stream()
+                    .allMatch(col -> col.getCards().stream()
+                            .allMatch(card -> !card.getCard().isShown())));
+    }
+
+    public static boolean allCardsShown(@This SkyJoSet set) {
+        return set.getPlayerSets().stream()
+            .allMatch(ps -> ps.getColumns().stream()
+                    .allMatch(col -> col.getCards().stream()
+                            .allMatch(card -> card.getCard().isShown())));
+    }
+
+    public static boolean isPlaying(@This SkyJoSet set, String player) {
+        return set.getPlayerSets().stream()
+            .filter(p -> Objects.equals(p.getPlayer().getName(), player))
+            .findFirst()
+            .map(PlayerSet::isPlaying)
+            .orElseThrow(() -> new IllegalStateException("Wrong player name"));
+    }
+}
